840d5dc8543d2012f8a184c7cd4d031d8b1fa253
bkcoder09 <80070457+bkcoder09@users.noreply.github.com>
2022-03-13 16:02:59 -0400

    Add files via upload

---
+++ argo.js
@@ -0,0 +1,31 @@
+function home(){
+    window.location="index.html";
+}
+
+function gods(){
+    window.location="god.html";
+}
+
+function goddesses(){
+    window.location="goddess.html";
+}
+
+function war(){
+    window.location="war.html";
+}
+
+function hades(){
+    window.location="hades.html";
+}
+
+function calypso(){
+    window.location="calypso.html";
+}
+
+function fates(){
+    window.location="fates.html";
+}
+
+function furies(){
+    window.location="furies.html";
+}
---
+++ hades.js
@@ -0,0 +1,31 @@
+function home(){
+    window.location="index.html";
+}
+
+function gods(){
+    window.location="god.html";
+}
+
+function goddesses(){
+    window.location="goddess.html";
+}
+
+function war(){
+    window.location="war.html";
+}
+
+function jason(){
+    window.location="argo.html";
+}
+
+function calypso(){
+    window.location="calypso.html";
+}
+
+function fates(){
+    window.location="fates.html";
+}
+
+function furies(){
+    window.location="furies.html";
+}
